4e1aae192566781ea347752938517255f725c167
Marcus Bloomfield <marcusalanbloomfield@gmail.com>
2025-06-25 16:57:50 +0800

    Fixed blog post pointing to incorrect steam post

--- src/components/DevBlog.vue
+++ src/components/DevBlog.vue
@@ -8,7 +8,7 @@
                 <div class="blog-post-date">Wed, June 18, 2025</div>
                 <div class="blog-post-title">Tech, Roads and Buildings!</div>
                 <div class="blog-post-excerpt">I've implemented the first iteration of the road network, these roads are not only visual they boost movement speed and also offer the citizen a more performant path finding allowing the game to run faster and help citizens navigate the world with ease. Additionally there is only one road network system, meaning all roads will be connected and will function as the life line of the city.</div>
-                <a href="https://store.steampowered.com/news/app/1334570/view/564749771124769801" class="blog-post-link" target="_blank" rel="noopener noreferrer">Read More</a>
+                <a href="https://store.steampowered.com/news/app/1334570/view/535478910131901782" class="blog-post-link" target="_blank" rel="noopener noreferrer">Read More</a>
             </div>
 
             <div class="blog-post">
